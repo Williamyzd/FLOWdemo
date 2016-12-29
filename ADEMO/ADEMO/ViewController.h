1a16a6925009632731629075c8064d5f391d29ad
william <williamyzd@163.com>
2016-12-29 15:14:39 +0800

    提交

---
+++ ADEMO/ADEMO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADEMO
+//
+//  Created by william on 2016/12/29.
+//  Copyright © 2016年 william. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ADEMO/ADEMO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ADEMO
+//
+//  Created by william on 2016/12/29.
+//  Copyright © 2016年 william. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
